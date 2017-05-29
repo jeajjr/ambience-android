ae333a15af4156fdd8db7e0f6968f0487fd1f78b
Jose Almas <jeajjr@gmail.com>
2017-05-29 19:16:30 -0300

    Added colorpicker; reorganized hard coded IP:PORT

--- app/src/main/java/tk/jealmas/ambience/ActivityMain.java
+++ app/src/main/java/tk/jealmas/ambience/ActivityMain.java
@@ -2,24 +2,30 @@ package tk.jealmas.ambience;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.SeekBar;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.flask.colorpicker.ColorPickerView;
+import com.flask.colorpicker.slider.LightnessSlider;
 
 import tk.jealmas.ambience.data.ConfigTransmitter;
 
 public class ActivityMain extends Activity {
+    private static final String TAG = ActivityMain.class.getName();
 
+    // UI Components
     private Button commandButton;
-    private EditText textIp;
-    private TextView commandText;
+    private ProgressBar progressBar;
+    private ColorPickerView cpv;
+    private LightnessSlider ls;
+    private ImageView status;
 
-    private SeekBar barRed;
-    private SeekBar barGreen;
-    private SeekBar barBlue;
-
+    // Controll variables
     private int red = 0;
     private int green = 0;
     private int blue = 0;
@@ -29,34 +35,46 @@ public class ActivityMain extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        cpv = (ColorPickerView) findViewById(R.id.color_picker_view);
+        ls = (LightnessSlider) findViewById(R.id.lightness_slider);
+        cpv.setLightnessSlider(ls);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        status = (ImageView) findViewById(R.id.status);
         commandButton = (Button) findViewById(R.id.buttonCommand);
-
-        textIp = (EditText) findViewById(R.id.textIp);
-
-        barRed = (SeekBar) findViewById(R.id.barRed);
-        barGreen = (SeekBar) findViewById(R.id.barGreen);
-        barBlue = (SeekBar) findViewById(R.id.barBlue);
-
-        commandText = (TextView) findViewById(R.id.textCommand);
 
         commandButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                red = barRed.getProgress();
-                green = barGreen.getProgress();
-                blue = barBlue.getProgress();
+
+                int color = cpv.getSelectedColor();
+                red = (int) (((color & 0xFF0000) >> 16)*4*ls.getAlpha());
+                green = (int) (((color & 0x00FF00) >> 8)*4*ls.getAlpha());
+                blue = (int) ((color & 0x0000FF)*4*ls.getAlpha());
+
+                Log.d(TAG, "color " + color);
 
                 (new ConfigTransmitter(new ConfigTransmitter.ConfigTransmitterListener() {
                     @Override
                     public void onSuccess() {
-                        commandText.setText("OK");
+                        status.setImageResource(R.drawable.success);
                     }
 
                     @Override
                     public void onFail() {
-                        commandText.setText("FALHA");
+                        status.setImageResource(R.drawable.fail);
                     }
-                }, textIp.getText().toString(), red, green, blue)).execute();
+
+                    @Override
+                    public void onStart() {
+                        progressBar.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        progressBar.setVisibility(View.INVISIBLE);
+                    }
+                },red, green, blue)).execute();
             }
         });
     }
--- app/src/main/java/tk/jealmas/ambience/data/ConfigTransmitter.java
+++ app/src/main/java/tk/jealmas/ambience/data/ConfigTransmitter.java
@@ -9,16 +9,19 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class ConfigTransmitter extends AsyncTask<Void, Void, Boolean> {
+    private static final String TAG = ConfigTransmitter.class.getName();
+
+    private static String IP = "192.168.0.10";
+    private static Integer PORT = 45225;
+    private static Integer TIMEOUT = 5;
 
     private ConfigTransmitterListener configTransmitter;
-    private String IP;
     private int red;
     private int green;
     private int blue;
 
-    public ConfigTransmitter(ConfigTransmitterListener configTransmitter, String IP, int red, int green, int blue) {
+    public ConfigTransmitter(ConfigTransmitterListener configTransmitter, int red, int green, int blue) {
         this.configTransmitter = configTransmitter;
-        this.IP = IP;
         this.red = red;
         this.green = green;
         this.blue = blue;
@@ -27,36 +30,44 @@ public class ConfigTransmitter extends AsyncTask<Void, Void, Boolean> {
     public interface ConfigTransmitterListener {
         void onSuccess();
         void onFail();
+        void onStart();
+        void onFinish();
     }
 
+    @Override
+    protected void onPreExecute() {
+        configTransmitter.onStart();
+    }
 
     @Override
     protected Boolean doInBackground(Void... params) {
         try {
-            Socket socket = new Socket(IP, 45225);
+            Socket socket = new Socket(IP, PORT);
+
+            socket.setSoTimeout(TIMEOUT);
             DataOutputStream dOut = new DataOutputStream(socket.getOutputStream());
 
-            Log.d("ConfigTransmitter", "got connection, sending " + red + "," + green + "," + blue);
+            Log.d(TAG, "got connection, sending " + red + "," + green + "," + blue);
 
             dOut.writeInt(red);
             dOut.writeInt(green);
             dOut.writeInt(blue);
 
-            Log.d("ConfigTransmitter", "sent message");
+            Log.d(TAG, "sent message");
 
             DataInputStream dIn = new DataInputStream(socket.getInputStream());
             int input = dIn.readInt();
 
-            Log.d("ConfigTransmitter", "got message " + input);
+            Log.d(TAG, "got message " + input);
 
             socket.close();
 
             return true;
 
         } catch (IOException e) {
-            Log.e("ConfigTransmitter", "exception", e);
+            Log.e(TAG, "exception", e);
         } catch (Exception e) {
-            Log.e("ConfigTransmitter", "exception", e);
+            Log.e(TAG, "exception", e);
         }
 
         return false;
@@ -64,6 +75,8 @@ public class ConfigTransmitter extends AsyncTask<Void, Void, Boolean> {
 
     @Override
     protected void onPostExecute(Boolean aBoolean) {
+        configTransmitter.onFinish();
+
         if (aBoolean)
             configTransmitter.onSuccess();
         else
